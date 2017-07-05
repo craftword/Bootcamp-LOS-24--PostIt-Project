1c197fe1d2ce14bf3a12034e144a6af53b3c21d6
Abimbola Olaitan <craftwordltd@gmail.com>
2017-07-05 23:45:01 +0100

    fix issues with sass

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,10 @@ let sass = require('gulp-sass');
 
 gulp.task('styles', function() {
     gulp.src('template/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed',
+          includePaths: ['node_modules/susy/sass', 'node_modules/compass-mixins/lib', 'node_modules/breakpoint-sass/stylesheets']
+        }).on('error', sass.logError))
         .pipe(gulp.dest('./template/css/'));
 });
 
